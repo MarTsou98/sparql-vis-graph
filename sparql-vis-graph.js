873aec228b7912a4208588db74d294a8a7065ec4
MarTsou98 <73964623+MarTsou98@users.noreply.github.com>
2023-12-03 19:54:51 +0200

    Add files via upload

---
+++ sparql-vis-graph.js
@@ -0,0 +1,19 @@
+var query;
+var iri;
+var conn; //connection string
+function string_Builder() {
+  // Sets uo the iri and query to be used;
+}
+function exec_Query() {
+  // execute query and get results.
+}
+
+function changeFrom_SingleToDouble() {
+  //Change "" to '' from a string.
+}
+function activate_Graph_Area() {
+  //change visibility of graph area when there is a graph to be shown.
+}
+function Hello() {
+  alert("Hello");
+}
